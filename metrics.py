e1009f48a2ed62b899ee9070b6f717834213ebbf
Stevellen <belcher.steven@gmail.com>
2022-05-14 21:15:06 -0500

    confusion matrix update

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 import torchmetrics as tm
+import matplotlib.pyplot as plt
 
 METRIC_CONFIG = {
     'train': {
@@ -20,5 +21,18 @@ def get_metrics(threshold, beta=1):
     )
     return metrics.clone(prefix='train_'), metrics.clone(prefix='val_'), metrics.clone(prefix='test_')
 
-def get_cmat(preds, labels, stage='test'):
-    return
+def get_cmat(matrix, stage):
+    fig, ax = plt.subplots(figsize=(13, 13))
+    ax.imshow(matrix)
+    ax.set_title(f"{stage} Confusion Matrix", fontsize=30)
+    ax.set_xticks([0,1])
+    ax.set_yticks([0,1])
+    ax.set_xticklabels(['Negative', 'Positive'])
+    ax.set_yticklabels(['Negative', 'Positive'])
+
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            ax.text(j, i, matrix[i, j],
+            ha='center', va='center', color='black', fontsize=40)
+    fig.tight_layout()
+    return fig
